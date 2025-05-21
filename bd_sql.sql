53948ebd6af9b41159bc300bbc54df5bff5c20a1
Luan334 <luan_augusto34@hotmail.com>
2025-05-21 17:54:42 -0300

    Terceiro commit

--- bd_sql.sql
+++ bd_sql.sql
@@ -2,19 +2,17 @@ CREATE DATABASE testdb;
 
 USE testdb;
 
+
 CREATE TABLE teste (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(255)
-
-const sql = 'SELECT * FROM teste';
-
+  nome VARCHAR(255),
+  cpf VARCHAR(14),
+  telefone VARCHAR(15)
 );
 
 
 
-
-
-// USE testdb;
+ USE testdb;
+ DESCRIBE teste;
 SHOW TABLES;
 SELECT * FROM teste;
-DESCRIBE teste; //
